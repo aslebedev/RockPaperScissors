eca9d9007de59ce48041a77c1b1d3c679fe52483
Alexander Lebedev <alexander.s.lebedev@gmail.com>
2019-10-20 12:48:40 +0700

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by alexander on 20.10.2019.
+//  Copyright © 2019 alexander. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
